a8505be93200781831bec9e77babf25dda64c10f
d3ltaaa <hil.falk@protonmail.com>
2023-05-08 13:21:47 +0200

    lf-themes

--- config/suckless/themes/theme-arch-1.h
+++
@@ -1,5 +0,0 @@
-// ARCH-1
-
-#define MAIN_COLOR "#2c3440"
-#define SECOND_COLOR "#767b8a"
-#define THIRD_COLOR "#f0f2f7"
--- config/suckless/themes/theme-arch-2.h
+++
@@ -1,5 +0,0 @@
-// ARCH-2
-
-#define MAIN_COLOR "#eff3fa"
-#define SECOND_COLOR "#767b8a"
-#define THIRD_COLOR "#2c3440"
