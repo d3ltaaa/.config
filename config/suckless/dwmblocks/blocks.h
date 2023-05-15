2306e9bd30caa5d82e444cf276b47ed2df59e4aa
d3ltaaa <hil.falk@protonmail.com>
2023-05-15 11:39:04 +0200

    scripts 2

--- config/suckless/dwmblocks/blocks.h
+++ config/suckless/dwmblocks/blocks.h
@@ -1,16 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
-//a
-#include "/home/falk/.config/suckless/color.h"
 
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"",        "sb-volume",    0,                  1               },
-    {"",        "sb-net",       0,                  5               },
-    {"",        "sb-blue",      0,                  2               },
-    {"",        "sb-mail",      0,                  6,              },
-    {"",        "sb-clock",     60,                 3               },
-    {"",        "sb-battery",   300,                7,              },
-    {"",        "sb-system",    0,                  4               },
+    {"",        "sb_volume",    0,                  1               },
+    {"",        "sb_net",       0,                  5               },
+    {"",        "sb_blue",      0,                  2               },
+    {"",        "sb_mail",      0,                  6,              },
+    {"",        "sb_clock",     60,                 3               },
+    {"",        "sb_battery",   300,                7,              },
+    {"",        "sb_system",    0,                  4               },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
--- system/.bash_profile
+++ system/.bash_profile
@@ -7,6 +7,8 @@ export PATH="$PATH:$HOME/.scripts/app_names/"
 export PATH="$PATH:$HOME/.scripts/system_scripts/"
 export PATH="$PATH:$HOME/.scripts/theme_scripts/"
 export PATH="$PATH:$HOME/.scripts/dwmblocks_scripts/"
+export PATH="$PATH:$HOME/.scripts/test_scripts/"
+
 
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 
